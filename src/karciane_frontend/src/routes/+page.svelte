8f4037fe8ba0646564f3d3f539d31e40a5ebd54b
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2024-09-29 22:24:55 +0200

    przykład z konsolą i F12

--- src/karciane_frontend/src/routes/+page.svelte
+++ src/karciane_frontend/src/routes/+page.svelte
@@ -4,13 +4,16 @@
 
   let greeting = "";
 
-  function onSubmit(event) {
+   function onSubmit(event) {
     const name = event.target.name.value;
     backend.greet(name).then((response) => {
       greeting = response;
     });
     return false;
   }
+
+  $: console.log({greeting});
+
 </script>
 
 <main>
@@ -18,9 +21,11 @@
   <br />
   <br />
   <form action="#" on:submit|preventDefault={onSubmit}>
-    <label for="name">Enter your name: &nbsp;</label>
+    <label for="name">Wynik WE: &nbsp;</label>
     <input id="name" alt="Name" type="text" />
     <button type="submit">Click Me!</button>
   </form>
   <section id="greeting">{greeting}</section>
+
 </main>
+ 
