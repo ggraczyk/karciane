4f962b7f53b963ca56c9cf9b88526013945bf375
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2024-10-02 23:16:29 +0200

    walidacja bliżej docelowej

--- src/karciane_frontend/src/routes/+layout.svelte
+++ src/karciane_frontend/src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <nav>
 	<a href="/">Home</a>
 	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
 </nav>
 
 
--- src/karciane_frontend/src/routes/+page.svelte
+++ src/karciane_frontend/src/routes/+page.svelte
@@ -1,10 +1,25 @@
 <script>
   import "../index.scss";
   import { backend } from "$lib/canisters";
+  import * as yup from 'yup';
 
+  let values = {};
+	let errors = {};
+
+    const schema = yup.object().shape({
+      name: yup.string().required("uzupełnij pole").matches(/^[-,0-9]+5$|0$/,
+                            "tylko liczby podzielne przez 5!"
+    ),
+    });
+
+   
+  
   let greeting = "";
 
    function onSubmit(event) {
+
+    alert(JSON.stringify(values));
+
     const name = event.target.name.value;
     backend.greet(name).then((response) => {
       greeting = response;
@@ -12,7 +27,28 @@
     return false;
   }
 
-  $: console.log({greeting});
+
+  function extractErrors(err) {
+		return err.inner.reduce((acc, err) => {
+			return { ...acc, [err.path]: err.message };
+		}, {});
+	}
+
+  async function submitHandler() {
+		try {
+			// `abortEarly: false` to get all the errors
+			await schema.validate(values, { abortEarly: false });
+			//alert(JSON.stringify(values, null, 2));
+			errors = {};
+		} catch (err) {
+			errors = extractErrors(err);
+		}
+
+
+	}
+
+
+  // $: console.log({greeting});
 
 </script>
 
@@ -20,11 +56,14 @@
   
   <br />
   <br />
-  <form action="#" on:submit|preventDefault={onSubmit} >
+  <form action="#" on:submit|preventDefault={submitHandler}>
     <label for="name">Wynik WE: &nbsp;</label>
-    <input id="name" alt="Name" type="text" required />
+  
+    <input id="name" alt="Name" type="text" bind:value={values.name}/>
+    <span>{#if errors.name}{errors.name}{/if}</span>
     <button type="submit">Click Me!</button>
   </form>
+  
   <section id="greeting">{greeting}</section>
 
 </main>
