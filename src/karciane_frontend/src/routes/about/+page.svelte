9f3c8ece1094958cb689f42cdef52b9257605ce2
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2024-10-23 00:53:25 +0200

    docs(todo): :bulb:

--- src/karciane_frontend/src/routes/about/+page.svelte
+++ src/karciane_frontend/src/routes/about/+page.svelte
@@ -1,2 +1,14 @@
+<section>
 <h1>About this site</h1>
 <p>TODO...</p>
+<li>możliwość kasowania pojedynczej partii</li>
+<li>zapamiętanie historycznie całych rozgrywek</li>
+<li>NS/WE -> na edytowalne inicjały</li>
+<li>dodać brydż rober</li>
+<li>dodać brydż chicago</li>
+</section>
+<style>
+section {
+    font-size: 0.9rem;
+    }
+</style>
