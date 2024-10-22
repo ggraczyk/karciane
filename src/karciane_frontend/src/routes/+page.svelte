b2750c4c99a3fecc0a21319fc92226933aa09dfa
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2024-10-23 00:22:01 +0200

    feat(gui): :art: kandydat na betę
    kolorowanie, układ, legenda

--- src/karciane_frontend/src/routes/+page.svelte
+++ src/karciane_frontend/src/routes/+page.svelte
@@ -2,13 +2,14 @@
   import "../index.scss";
   import { backend } from "$lib/canisters";
   import * as yup from 'yup';
-  import { fade, scale } from 'svelte/transition';
+  import { fade, blur, fly, slide, scale, draw , crossfade } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
 
   let values = {};
 	let errors = {};
   let sumaNS = 0;
   let sumaWE = 0;
+  let blokujDodaj = false;
  
   const sleep = ms => new Promise(f => setTimeout(f, ms));
 
@@ -42,6 +43,7 @@
 		return err.inner.reduce((acc, err) => {
 			return { ...acc, [err.path]: err.message };
 		}, {});
+    
 	}
 
 
@@ -55,11 +57,14 @@ async function czytajWyniki() {
           sumaNS += wyniki[i].ns;
           sumaWE += wyniki[i].we;
 	      };
+        blokujDodaj=false;
   }
+  
 )};
 
 
   async function submitHandler() {
+    blokujDodaj=true;
 		try {
 			// `abortEarly: false` to get all the errors
 			await schema.validate(values, { abortEarly: false });
@@ -74,6 +79,7 @@ async function czytajWyniki() {
       await czytajWyniki();
   
 		} catch (err) {
+      blokujDodaj=false;
 			errors = extractErrors(err);
 		}
 
@@ -89,21 +95,23 @@ async function czytajWyniki() {
 <main>
   
   <br />
-  <br />
+
   <form action="#" on:submit|preventDefault={submitHandler}>
-    <label for="pair1">Wynik WE: &nbsp;</label>
+    <label for="pair1">Wynik WE: </label>
     <input id="pair1" alt="pair1" type="text" bind:value={values.pair1}/>
     <span>{#if errors.pair1}{errors.pair1}{/if}</span>
-    <label for="pair2">Wynik NS: &nbsp;</label>
+     <label for="pair2">Wynik NS: </label>
     <input id="pair2" alt="pair2" type="text" bind:value={values.pair2}/>
     <span>{#if errors.pair2}{errors.pair2}{/if}</span>
-    <button type="submit">Dodaj</button>
+    {#if blokujDodaj}
+      <div><button type="submit" hidden>Dodaj</button></div>
+    {:else}
+      <div><button type="submit">Dodaj</button></div>
+    {/if}
   </form>
   <br />
 
-  <button on:click={czytajWyniki}>
-	    Czytaj 
-  </button>
+
 
   <br /> 
 
@@ -133,19 +141,114 @@ async function czytajWyniki() {
     {/each} -->
 
 
-    <div class= "array">
+    <div class= "array" >
       {#each wyniki as element, i (element)}
-        <div animate:flip="{{ duration: 300 }}" out:scale="{{ duration: 250 }}" in:scale="{{ duration: 250 }}" class="element">
-          { + element.ns + "..........." + element.we }
+
+        <div animate:flip="{{ duration: 300 }}" out:scale="{{ duration: 250 }}" in:scale="{{ duration: 1250 }}">
+          {#if wyniki.length - i < 5}
+           <span id= "minusy">{#if element.ns<0}{"" + element.ns  }{/if}</span>
+           <span          >{#if element.ns>=0}{"" + element.ns  }{/if}</span>
+            { "........."  }
+            <span id= "minusy">{#if element.we<0}{"" + element.we  }{/if}</span>
+            <span          >{#if element.we>=0}{"" + element.we  }{/if}</span>
+          {/if}
         </div>
       {/each}
-    </div>  
-    ------------------------<br />
-    {sumaNS}........{sumaWE}
-    {/if}
-	</section>
+<br />
 
+     </div>  
+
+
+    {/if}
+
+
+
+  </section>
+  
+  <div id="suma" transition:blur={{ amount: 100,duration: 2900 }} >&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    <span id= "minusyP"   >{#if sumaNS<=0}{"" + sumaNS  }{/if}</span>
+    <span id= "pierwszyP">{#if sumaNS>0 &&sumaNS<1500}{"" + sumaNS  }{/if}</span>
+    <span id= "drugiP"   >{#if sumaNS>=1500 && sumaNS<3000}{"" + sumaNS  }{/if}</span>
+    <span id= "trzeciP"  >{#if sumaNS>=3000 && sumaNS<5000}{"" + sumaNS  }{/if}</span>
+    <span id= "wygranyP"  >{#if sumaNS>=5000}{"" + sumaNS  }{/if}</span>
+    ........
+    
+    <span id= "minusyP"   >{#if sumaWE<0}{"" + sumaWE  }{/if}</span>
+    <span id= "pierwszyP">{#if sumaWE>=0 &&sumaWE<1500}{"" + sumaWE  }{/if}</span>
+    <span id= "drugiP"   >{#if sumaWE>=1500 && sumaWE<3000}{"" + sumaWE  }{/if}</span>
+    <span id= "trzeciP"  >{#if sumaWE>=3000 && sumaWE<5000}{"" + sumaWE  }{/if}</span>
+    <span id= "wygranyP"  >{#if sumaWE>=5000}{"" + sumaWE  }{/if}</span>  
+  
+  </div>
   <br />
-  <br />
+
+  <div>
+  <button on:click={czytajWyniki} >
+    Czytaj 
+</button>
+</div>
+<span id= "minusyP"   font-size=10px >na 15</span>
+<span id= "pierwszyP">na 50</span>
+<span id= "drugiP"   >na 90</span>
+<span id= "trzeciP"  >na 120</span>
+<span id= "czwartyP"  >na 150</span>
+<span id= "piatyP"  >na kanastę</span>
+
+  <br /><br />
 </main>
+
+<style>
+#wyniki {
+  margin: 10px auto;
+  padding: 10px 60px;
+  border: 1px solid #222;
+  background-color:antiquewhite;
+  justify-content: center;
+}
+
+
+#suma {
+  color: #e00b0b;
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.5rem;
+  background-color:whitesmoke;
+  border: dashed;
+}
+
+#minusy {
+  color: #e00b0b;
+}
+#minusyP {
+  background-color: #8871ee;
+}
+#pierwszyP {
+  background-color:rgb(90, 221, 231);
+}
+
+#drugiP {
+  background-color: #37e334;
+}
+
+#trzeciP {
+  background-color: #d0d536;
+}
+
+#czwartyP {
+  background-color: #e7ab1e;
+}
+
+#piatyP {
+  background-color: #ec8123;
+}
+
+#wygranyP {
+  animation: blink 1s linear infinite;
+  }
+  @keyframes blink {
+    50% {
+      opacity: 0;
+    }
+}
+
+</style>
  
