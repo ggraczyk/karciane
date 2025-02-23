c5a913f243e35091fa2e618d0e468fbe518accf8
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2025-02-23 03:29:02 +0100

    powrot vite

--- src/karciane_frontend/vite.config.js
+++ src/karciane_frontend/vite.config.js
@@ -1,6 +1,39 @@
+import { fileURLToPath, URL } from 'url';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: '../../.env' });
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  build: {
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:4943",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [
+    sveltekit(),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
+  ],
+  resolve: {
+    alias: {
+      "declarations": path.resolve(__dirname, '../../declarations'),
+    },
+  },
 });
