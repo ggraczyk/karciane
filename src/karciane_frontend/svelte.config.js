4d001a89451bd0a161d56a3825f57a00533a6a53
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2025-02-23 12:14:29 +0100

    przywrócenie zawartości svelte.config.js

---
+++ src/karciane_frontend/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    })
+  },
+  preprocess: vitePreprocess()
+};
+
+export default config;
