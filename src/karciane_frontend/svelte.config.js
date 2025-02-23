0c9939f3f156bb33b0b35a3d21ab8c5a75c0565c
Graczyk Grzegorz <grzegorz.graczyk@asseco.pl>
2025-02-23 11:42:10 +0100

    zmiany svelte.config.js

--- src/karciane_frontend/svelte.config.js
+++ src/karciane_frontend/svelte.config.js
@@ -1,11 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       pages: 'dist',
       assets: 'dist',
@@ -13,7 +11,12 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    prerender: {
+      enabled: false, // Wy³¹cz prerendering dla strony z akcjami
+    },
+    csr: true, // W³¹cz Client-Side Rendering
   },
+  preprocess: vitePreprocess()
 };
 
 export default config;
